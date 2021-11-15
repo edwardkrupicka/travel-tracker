de11fd42a06dcaa0205504cf67eb02daa3a57bb7
Edward Krupicka <edwardkrupicka@gmail.com>
2021-11-15 15:25:04 -0700

    Add basic error handling

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,31 +1,49 @@
+import {connectionErr, checkStatus} from "./error-handling.js";
 
-const getAllTravelerData = () => {
-  return fetch('http://localhost:3001/api/v1/travelers')
-  .then((response) => response.json())
-  .catch(error => console.log(error) )
+function getData(dataUrl, dataObjName) {
+  const retrievedData = fetch(dataUrl)
+    .then(res => {
+      checkStatus(res, `There was an error connecting to ${dataUrl}.`)
+      return res.json();
+    })
+    .then(data => data[dataObjName])
+    .catch(err => {
+      if (err.message === "Failed to fetch") {
+        connectionErr(err, "Couldn't connect to database.")
+      } else {
+        connectionErr(err);
+      }
+    });
+  return retrievedData;
 }
 
-const getSingleTravelerData = () => {
-  return fetch(`http://localhost:3001/api/v1/travelers/${id}`)
-  .then((response) => response.json())
-  .catch(error => console.log(error) )
+function getAllData() {
+  const gotTravelerData = getData('http://localhost:3001/api/v1/travelers/', 'travelers');
+  const gotAllTripData = getData('http://localhost:3001/api/v1/trips', 'trips');
+  const gotAllDestinationData = getData('http://localhost:3001/api/v1/destinations', 'destinations');
+  const allPromise = Promise.all([gotTravelerData, gotAllTripData, gotAllDestinationData]).then(data => data);
+  return allPromise;
 }
 
-const getAllTripData = () => {
-  return fetch('http://localhost:3001/api/v1/trips')
-  .then((response) => response.json())
-  .catch(error => console.log(error) )
-}
+// function postData(url, newData) {
+//   console.log(newData.date)
+//   const postedData = fetch(url, {
+//     method: "POST",
+//     body: JSON.stringify(newData),
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   })
+//   .then(response => {
+//     checkStatus(response);
+//     return response.json();
+//   })
+//   .catch(err => connectionErr(err));
+//   return postedData;
+// }
 
-const getAllDestinationData = () => {
-  return fetch('http://localhost:3001/api/v1/destinations')
-  .then((response) => response.json())
-  .catch(error => console.log(error) )
-}
+export { getAllData }
 
-export {
-  getAllTravelerData,
-  getSingleTravelerData,
-  getAllTripData,
-  getAllDestinationData
-};
+
+
+
