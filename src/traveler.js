5d12029eda83ed2e9ddb9f11f1ca81721f12243f
Edward Krupicka <edwardkrupicka@gmail.com>
2021-11-15 11:44:54 -0700

    Add functionality for tests

--- src/traveler.js
+++ src/traveler.js
@@ -1,11 +1,31 @@
-
+import Trip from './Trip';
 
 class Traveler {
-    constructor(allTravelersData, allTripData, allDestinationData) {
-        this.id = allTravelersData.id;
-        this.name = allTravelersData.name;
-        this.travelerType = allTravelersData.travelerType;
-    }
-}
+  constructor(allTravelersData, allTripData, allDestinationData) {
+    this.id = allTravelersData.id;
+    this.name = allTravelersData.name;
+    this.travelerType = allTravelersData.travelerType;
+    this.tripData = this.getTravelerTripData(allTripData, allDestinationData);
+  }
+
+  getTravelerTripData(allTripData, allDestinationData) {
+    return allTripData.reduce((arr, trip) => {
+      if(trip.userID === this.id) {
+        const tripInstance = new Trip(trip, allDestinationData);
+        arr.push(tripInstance);
+      }
+      return arr
+    }, [])
+  };
+
+  calculateSpentThisYear() {
+    return this.tripData.reduce((acc, trip) => {
+      if(trip.date.includes('2021')) {
+        acc += trip.calculateTripCost();
+      }
+      return acc;
+    }, 0)
+  }
+};
 
 export default Traveler;
