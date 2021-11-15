0739333c718c11de3b900b31979004fa3e3b5adf
Edward Krupicka <edwardkrupicka@gmail.com>
2021-11-15 15:26:59 -0700

    Add basic error handling set up

---
+++ src/error-handling.js
@@ -0,0 +1,22 @@
+const errorMessage = document.querySelector('#errorMessage');
+const errorModal = document.querySelector('#errorModal');
+const errorButton = document.querySelector('#errorButton');
+
+const hideErrorModal = () => {
+  errorModal.classList.add('hidden');
+}
+
+const connectionErr = (err, customMessage) => {
+  errorMessage.innerText = customMessage || err.message;
+  errorModal.classList.remove('hidden');
+}
+
+const checkStatus = (res, customMessage) => {
+  if (!res.ok) {
+    throw new Error(customMessage);
+  } 
+}
+
+errorButton.addEventListener("click", hideErrorModal);
+
+export {connectionErr, checkStatus};
